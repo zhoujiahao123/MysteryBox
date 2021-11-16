6bb12246790da373ee630572ee5adfa70fa1249a
zhoujiahao123 <zhoujiahao@foxmail.com>
2021-11-16 15:34:18 +0800

    first commit

---
+++ src/test/java/com/jacob/mysterybox/MysteryboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacob.mysterybox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysteryboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
