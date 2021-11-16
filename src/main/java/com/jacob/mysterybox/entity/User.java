b8dbf8b27e811ff304203f237b671e7fc7277bce
zhoujiahao123 <zhoujiahao@foxmail.com>
2021-11-16 15:48:51 +0800

    create user class

---
+++ src/main/java/com/jacob/mysterybox/entity/User.java
@@ -0,0 +1,53 @@
+package com.jacob.mysterybox.entity;
+
+/**
+ * @author jacob
+ * @date 2021/11/16 15:46
+ */
+public class User {
+    private String name;
+    private String mobileNumber;
+    private String address;
+    private char gender;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
